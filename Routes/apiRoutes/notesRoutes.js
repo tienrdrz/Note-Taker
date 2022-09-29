abfc87a6dc5ee1b381ae8ea601a40f62d38af514
etienne <etiennerodriguez3@gmail.com>
2022-09-29 09:32:58 -0400

    getting an error 'MIME type' when trying to load js and css

--- Routes/apiRoutes/notesRoutes.js
+++ Routes/apiRoutes/notesRoutes.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
-const { db } = require('../../Develop/db/db.json');
+const { addNote, findNoteById, delNote } = require('../../lib/notes');
 
-// Will be called in POST to add a note
-const addNote = require('../../lib/notes')
+//this references the the json body in db.json
+const { notes } = require('../../Develop/db/db.json');
+
+// // Will be called in POST to add a note
+// const addNote = require('../../lib/notes')
 
 // GET the notes in db
-router.get('/notes', (req, res) => {
-    res.json(db);
+router.get('/notes/:id', (req, res) => {
+    const result = findNoteById(req.params.id, notes);
+
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404).json({ message: "Error"});
+    }
 });
 
 //POST a new note
@@ -19,5 +28,13 @@ router.post('/notes', (req, res) => {
 });
 
 //DELETES a new note
+router.delete('/notes/:id', (req, res) => {
+delNote(req.params.id, notes);
+if (delNote) {
+    res.json(notes);
+} else {
+    res.status(404).json({message: "error"});
+}
+});
 
 module.exports = router;
